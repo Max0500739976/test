ce96e8782526e7a8522335ea6d5249d90e5ae0ac
Max0500739976 <85582512+Max0500739976@users.noreply.github.com>
2022-11-18 15:28:43 +0200

    Update TRANING.py

--- TRANING.py
+++ TRANING.py
@@ -1,41 +1,21 @@
-#aa = open('ltest2.txt')
-a = 'газонокосилка'# ввод с клавиатуры    .count(7)
-
-b = 'газон'# косилка '#aa.read()##'косилка'
-c = 'алиса'
-d = ['алиса\nгазон\nкосилка\nзона']#\nальфредо']
-'''
-
-газон солона
-ариня
-:xp1
-#
-танок,
-квітка
-аакуватий
-абажур
-абажурний
-абажурчик
-абаз
-абазія
-абайя
-:ua_1992
-'''
-
+aa = open('ltest2.txt')
+a = input('Введіть слово - ')#'газонокосилка'# ввод с клавиатуры    .count(7)
+d = aa.read()
+aa.close()
 def ravno(a,b):
-    stroka = ''
-    aa = list(a)
-    for i in b:
-        if i in a:
-            aa.remove(i)
-            stroka += i
-    if stroka == b:
-        return b
-    else:
-        None
-print(d)
-for i in d:
-
-    print(ravno(a,i))
+    for k in b:
+        if b.count(k) > a.count(k):
+            return False
+            break
+    return b
+dd = list(set(d.split('\n')))
+click = 0
+swsw = []
+for i in range(len(dd)):
+    if ravno(a,dd[i]) != False and int(len(dd[i]) > 2):
+        click += 1
+        print(click,' - ',ravno(a,dd[i]))
+        swsw = ravno(a,dd[i])  #split('\n')
+print(type(dd))
 
 
